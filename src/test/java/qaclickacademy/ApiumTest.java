21f612285575daf590f3f7983e0cd012538ba0db
ioangeorgiu <ioan.georgiu@yahoo.com>
2019-06-23 10:39:56 +0300

    first commit

---
+++ src/test/java/qaclickacademy/ApiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.junit.Test;
+
+public class ApiumTest {
+
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void iOSApps()
+	{
+		System.out.println("iOSApps");
+
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.junit.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation() 
+	{
+		System.out.println("BrowserAutomation");
+
+	}
+	
+	@Test
+	public void elementsUI() 
+	{
+		System.out.println("elementsUI");
+	}
+}
